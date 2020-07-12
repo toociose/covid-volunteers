1107cfb5974a84a6320577a8f4557eccc21dee1e
Camden Rogers <camden.rogers@ihg.com>
2020-07-12 15:39:45 -0400

    Updated default from

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "#{CITY_NAME} Help With Covid <#{HWC_NOREPLY}>"
+  #default from: "#{CITY_NAME} Help With Covid <#{HWC_NOREPLY}>"
+  default from: "#{HWC_NOREPLY}"
   layout 'mailer'
 end
