1eaa02ddf0cf77a5ebf0fd21d0e4186cbf98454e
Camden Rogers <camden.rogers@ihg.com>
2020-07-13 22:11:22 -0400

    Tweak notification email

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -3,12 +3,12 @@ class ProjectMailer < ApplicationMailer
     @project = params[:project]
     @user = params[:user]
 
-    mail(to: @project.user.email, bcc: 'camden@athenshelpwithcovid.com', subject: "You got a new volunteer for #{@project.name}!")
+    mail(to: @project.user.email, bcc: ADMINS, subject: "You got a new volunteer for #{@project.name}!")
   end
 
   def new_project
     @project = params[:project]
-    mail(to: @project.user.email, bcc: ADMINS, subject: "You created a new position: #{@project.name}!")
+    mail(to: @project.user.email, bcc: [ADMINS, 'sofi.rice97@gmail.com'], subject: "You created a new position: #{@project.name}!")
   end
 
   def volunteer_outreach
--- config/initializers/admins.rb
+++ config/initializers/admins.rb
@@ -1,5 +1,4 @@
 ADMINS = [
   'camdenrogers24@gmail.com',
-  'camden@athenshelpwithcovid.com',
-  'sofi.rice97@gmail.com'
+  'camden@athenshelpwithcovid.com'
 ]
