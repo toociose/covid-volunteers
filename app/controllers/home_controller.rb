debf3cd469e6ba8e18e5416690d2da615174684e
Mark Santolucito <mark.santolucito@yale.edu>
2020-07-16 08:47:29 -0400

    add button for all projects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,7 @@ class HomeController < ApplicationController
     @project_count = Rails.cache.fetch('project_count', expires_in: 1.day) do
       Project.count
     end
+    @project_count_total = @project_count
     # Display the projects in increments of 50
     @project_count = (@project_count / 50).floor * 50
 
